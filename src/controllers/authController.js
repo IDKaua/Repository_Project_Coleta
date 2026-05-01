cb426170a62ac6ac02ff0508e2c22229d2e8fe5d
IDKaua <cagaxes@gmail.com>
2026-04-30 21:14:53 -0300

    Limpando repositório de vez

--- src/controllers/authController.js
+++
@@ -1,7 +0,0 @@
-// Este é o Controller: apenas lógica, sem HTML!
-export const handleAccess = (tipo) => {
-  console.log(`[CONTROLLER] Acessando perfil: ${tipo}`);
-  alert(`Redirecionando para o painel de: ${tipo}`);
-  
-  // No futuro, aqui você fará o redirecionamento de rotas
-};
